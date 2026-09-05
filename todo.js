a37d81a5dd616ea5e4911e688436d0f739c5bb3a
Kenjie Autajay <kenjie030901@gmail.com>
2026-09-05 18:30:43 +0800

    Fixed the delete bug index

--- todo.js
+++ todo.js
@@ -5,34 +5,48 @@ let tasks = parsedTasks;
         for ( let i = 0; i < parsedTasks.length; i++ ){
         let li = document.createElement('li');
         let deleteBtn = document.createElement('button');
-        li.textContent = parsedTasks[i];     
+        let currentTask = parsedTasks[i];
+        li.textContent = currentTask.taskName;    
         deleteBtn.textContent = 'Delete';
         document.getElementById('taskList').appendChild(li);
         li.appendChild(deleteBtn);
         deleteBtn.addEventListener('click', function() {
-                tasks.splice(i,1);
+                tasks.splice(tasks.indexOf(currentTask), 1);
                 localStorage.setItem('tasks', JSON.stringify(tasks));
                 li.remove();
         });
         deleteBtn.classList.add('delete-btn');
         }; 
 
+
 document.getElementById('addBtn').addEventListener('click', function() {
         let taskText = document.getElementById('taskInput').value;
-        tasks.push(taskText);
+        
+
+if (taskText === ''){
+
+console.log("No character input");
+
+}
+
+else{
+        let newTask = { taskName: taskText, completed: false };
+        tasks.push(newTask);
         localStorage.setItem('tasks', JSON.stringify(tasks));
         let li = document.createElement('li');
         let deleteBtn = document.createElement('button');
-        li.textContent = taskText;     
+        li.textContent = newTask.taskName;
         deleteBtn.textContent = 'Delete';
         document.getElementById('taskList').appendChild(li);
         li.appendChild(deleteBtn);
         deleteBtn.addEventListener('click', function() {
-                tasks.splice(tasks.indexOf(taskText), 1);
+                tasks.splice(tasks.indexOf(newTask), 1);
                 localStorage.setItem('tasks', JSON.stringify(tasks));
                 li.remove();
         });
         document.getElementById('taskInput').value = '';
         
         deleteBtn.classList.add('delete-btn');
+
+}
 });
